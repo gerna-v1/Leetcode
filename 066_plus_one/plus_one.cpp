57f73e3251c445dc47c3df5edcb6b42a09d86847
gerna-v1 <gerardoelimar2@gmail.com>
2024-11-19 13:34:08 -0400

    Add better solution for problem 066: Plus One

--- 066_plus_one/plus_one.cpp
+++ 066_plus_one/plus_one.cpp
@@ -4,25 +4,60 @@
 class Solution {
 public:
     std::vector<int> plusOne(std::vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int number = digits[i] + 1;
+            if (number == 10 && i != 0) {
+                digits[i] = 0;
+            }
+            else {
+                digits[i] = number;
+                break;
+            }
+        }
+        if (digits[0] == 10) {
+            digits[0] = 1;
+            digits.push_back(0);
+        }
+        return digits;
+    }
+};
+
+// Previous solution, using strings:
+
+/* class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
         std::string number = "";
 
         for (int d : digits) {
-            number += (char) d + '0';
+            number += std::to_string(d);
         }
 
-        int integer = std::stoi(number);
+        int carry = 1;
+        for (int i = number.size() - 1; i >= 0; i--) {
+            int sum = (number[i] - '0') + carry;
+            if (sum == 10) {
+                number[i] = '0';
+                carry = 1;
+            }
+            else {
+                number[i] = sum + '0';
+                carry = 0;
+                break;
+            }
+        }
 
-        integer++;
+        if (carry == 1) {
+            number.insert(number.begin(), '1');
+        }
 
-        std::string number2 = std::to_string(integer);
-
-        std::vector<int> result = std::vector<int>();
-
-        for (int i = 0; i < number2.length(); i++) {
-            char digit = number2.at(i);
-            result.push_back(digit - '0');
+        std::vector<int> result;
+        for (char c : number) {
+            result.push_back(c - '0');
         }
 
         return result;
     }
-};
+}; */
